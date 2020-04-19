8a205716f3dd0c114aeb683cacf54f159fe093b9
Fido <cfrfido@gmail.com>
2020-04-19 18:42:40 +0200

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,11 @@
+import '../css/componentes.css';
+
+export const saludar = (nombre) => {
+
+    console.log('Creando etiqueta H1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}`;
+
+    document.body.prepend(h1);
+}
